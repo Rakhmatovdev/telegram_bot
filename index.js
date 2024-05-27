9c272badcb12cdf2876d7bbc438703c4014691fd
Jasur <rakhmatovjasur3@gmail.com>
2024-05-27 16:00:10 +0500

    mini Game bot

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const TelegramBot = require("node-telegram-bot-api");
+const { gameOptions, againOptions } = require("./options");
 const token = "6224853340:AAGu1RDHzSGSeozV0BTuvQg0sNzc7mDk7XA";
 const bot =new TelegramBot(token, { polling: true });
+
+
+const result={
+}
+
+
+
+
+const StartGame= async(chatId)=>{
+    await bot.sendSticker(chatId,"https://sl.combot.org/hangseed9/webp/15xf09f988c.webp")
+    await bot.sendMessage(chatId,"Kampyuter 0 dan 9 gacha son o'yladi shuni topishga harakat qiling ")
+    const randomNumber=Math.floor(Math.random()*10)
+    result[chatId]=randomNumber
+    await bot.sendMessage(chatId,"To'g'ri sonni toping ",gameOptions)
+}
+
+const botStart=()=>{
 
 bot.setMyCommands([
     {
@@ -14,14 +32,11 @@ bot.setMyCommands([
     {
         command:"/game",description:"Oyin o'ynaymizmi"
     }
-])
-
-
+])    
 
 bot.on("message", async (msg) => {
   const text = msg.text;
   const chatId = msg.chat.id;
-console.log(msg);
   if (text === "/start") {
       await bot.sendSticker(chatId,"https://sl.combot.org/spb8f996c4181c71758fe7e90fd10f0147_by_stckrrobot/webp/12xf09fa4a9.webp")
     return await bot.sendMessage(
@@ -40,5 +55,37 @@ if(text==="/info"){
     return bot.sendMessage(chatId,"Biz haqimizda qanday ma'lumotga egasiz?")
 }
 
+if(text==="/game"){
+return StartGame(chatId)
+}
+
 bot.sendMessage(chatId,"Uzr siznig gapingizga tushunmayapman ):")
-});
+});   
+
+bot.on('callback_query',async(msg)=>{
+const data =msg.data
+const chatId=msg.message.chat.id
+
+if(data ==="/again"){
+    return StartGame(chatId)
+}
+
+if(data == result[chatId]){
+await bot.sendSticker(chatId,"https://sl.combot.org/hangseed9/webp/11xf09f9283.webp")
+    return bot.sendMessage(chatId,`Ta'briklayman men shu  ${data} sonni oylagan edim !`,againOptions)
+
+}else{
+    await bot.sendSticker(chatId,'https://sl.combot.org/hangseed9/webp/14xf09f988d.webp')
+    return bot.sendMessage(chatId,`Afsuski men bu  ${data} sonni oylaman edim ! men ${result[chatId]} sonini oylagan edim `,againOptions)
+ 
+}
+
+
+bot.sendMessage(chatId,`Siz tanlagan son ${data}`)
+
+})
+}
+botStart()
+
+
+
